9fc9874e11fff320458a48cb3a6a177bffeeae4d
Jagdishuikey <161849549+Jagdishuikey@users.noreply.github.com>
2025-06-19 12:16:23 +0530

    Update StoreContext.jsx

--- potato/src/context/StoreContext.jsx
+++ potato/src/context/StoreContext.jsx
@@ -6,7 +6,7 @@ export const StoreContext = createContext(null)
 
 const StoreContextProvider = (props) => {
     const [cartItems, setCartItems] = useState({});
-    const url="https://tomato-backend-wc36.onrender.com"
+    const url="https://tomato-b-jsdj.onrender.com"
     const[token,setToken]=useState('');
     const[food_list,setFoodList]=useState([])
 
