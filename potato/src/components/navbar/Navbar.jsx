af57e22316c2b65468c8dfa4798aa9fdea7a8540
Jagdish Uikey <161849549+Jagdishuikey@users.noreply.github.com>
2025-12-03 22:31:37 +0530

    Update navbar to include 'MyOrders' text
    Added text 'MyOrders' to the profile dropdown menu.

--- potato/src/components/navbar/Navbar.jsx
+++ potato/src/components/navbar/Navbar.jsx
@@ -40,7 +40,7 @@ const Navbar = ({setShowLogin}) => {
             :<div className='navbar-profile'>
                   <img src={assets.profile_icon} alt="" />
                   <ul className='nav-profile-dropdown'>
-                    <li onClick={()=>navigate('/myorders')}><img src={assets.bag_icon} alt="" /></li>
+                    <li onClick={()=>navigate('/myorders')}><img src={assets.bag_icon} alt="" /><p>MyOrders</p>p></li>
                     <hr/>
                     <li onClick={logout}><img src={assets.logout_icon} alt="" /><p>Logout</p></li>
                   </ul>
